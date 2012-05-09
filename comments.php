c2f99bf71daf1cc4d2893551c8f08fee2805ca8b
Bryce <tonyhawk2100@gmail.com>
2012-05-09 12:22:49 -0700

    fix comments

--- comments.php
+++ comments.php
@@ -1,73 +1,49 @@
-/*
-Copyright (c) 2012 Bryce Campbell
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
 <?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); } ?>
-<?php if ( Session::has_messages() ) {
+<!-- comments -->
+		<hr>
+
+		<div class="comments">
+		<h4><span id="comments"><?php echo $post->comments->moderated->count; ?> <?php _e('Responses to', 'k2'); ?> <?php echo $post->title; ?></span></h4>
+		<div class="metalinks">
+			<span class="commentsrsslink"><a href="<?php echo $post->comment_feed_link; ?>"><?php _e('Feed for this Entry', 'k2'); ?></a></span>
+		</div>
+
+		<ol id="commentlist">
+<?php
+if ( $post->comments->moderated->count ) {
+	foreach ( $post->comments->moderated as $comment ) {
+
+?>
+			<li id="comment-<?php echo $comment->id; ?>" <?php echo $theme->k2_comment_class( $comment, $post ); ?>>
+			<a href="#comment-<?php echo $comment->id; ?>" class="counter" title="<?php _e('Permanent Link to this Comment', 'k2'); ?>"><?php echo $comment->id; ?></a>
+			<span class="commentauthor"><?php echo $comment->name_out; ?></span>
+			<small class="comment-meta"><a href="#comment-<?php echo $comment->id; ?>" title="<?php _e('Time of this Comment', 'k2'); ?>"><?php $comment->date->out(); ?></a><?php if ( $comment->status == Comment::STATUS_UNAPPROVED ) : ?> <em><?php _e('In moderation', 'k2'); ?></em><?php endif; ?></small>
+
+			<div class="comment-content">
+				<?php echo $comment->content_out; ?>
+
+			</div>
+			</li>
+
+<?php
+	}
+}
+else { ?>
+			<li><?php _e('There are currently no comments.', 'k2'); ?></li>
+<?php } ?>
+		</ol>
+
+<?php 	if ( ! $post->info->comments_disabled ) { ?>
+		<div class="comments">
+			<h4 id="respond" class="reply"><?php _e('Leave a Reply', 'k2'); ?></h4>
+<?php	if ( Session::has_messages() ) {
 		Session::messages_out();
 	}
-?>
-<!-- place to hold comments -->
-<div id="comments">
-<h3><?php $theme->comments_count($post,'%d Responses','%d Response','%d Responses'); ?> <?php _e('to'); ?> <?php echo $post->title; ?></h3>
-<a href="<?php echo $post->comment_feed_link; ?>"><?php _e("Feed For this entry"); ?></a>
-<?php if ($post->comments->pingbacks->count) { ?>
-<div id="pings">
-<h4><?php $theme->pingback_count($post); ?></h4>
-				<ul id="pings-list">
-					<?php foreach ( $post->comments->pingbacks->approved as $pingback ) { ?>
-						<li id="ping-<?php echo $pingback->id; ?>">
 
-								<div class="comment-content">
-								<?php echo $pingback->content; ?>
-								</div>
-								<div class="ping-meta"><a href="<?php echo $pingback->url; ?>" title=""><?php echo $pingback->name; ?></a></div>
-						</li>
-					<?php } ?>
-				</ul>
+	$post->comment_form()->out();
+} ?>
 </div>
-<?php } ?>
-<h4 class="commentheading"><?php echo $post->comments->comments->approved->count; ?> <?php echo _n( 'Comment', 'Comments', $post->comments->comments->approved->count ); ?></h4>
-	<ul id="commentlist">
+		<hr>
 
-		<?php
-		if ( $post->comments->moderated->count ) {
-			foreach ( $post->comments->comments->moderated as $comment ) {
-			$class = 'class="comment';
-			if ( $comment->status == Comment::STATUS_UNAPPROVED ) {
-				$class.= '-unapproved';
-			}
-			$class.= '"';
-		?>
-
-			<li id="comment-<?php echo $comment->id; ?>" <?php echo $class; ?>>
- 				<div class="comment-content">
-		        <?php echo $comment->content_out; ?>
-		       </div>
-			<div class="comment-meta">#<a href="#comment-<?php echo $comment->id; ?>" class="counter" title="<?php _e('Permanent Link to this Comment'); ?>"><?php echo $comment->id; ?></a> |
-		       <span class="commentauthor"><?php _e('Comment by'); ?> <?php $theme->comment_author_link($comment); ?></span>
-		       <span class="commentdate"> <?php _e('on'); ?> <a href="#comment-<?php echo $comment->id; ?>" title="<?php _e('Time of this comment'); ?>"><?php $comment->date->out('M j, Y h:ia'); ?></a></span><h5><?php if ( $comment->status == Comment::STATUS_UNAPPROVED ) : ?> <em><?php _e('In moderation'); ?></em><?php endif; ?></h5></div>
-		      </li>
-
-
-
-
-		<?php
-			}
-		}
-		else {
-			_e('There are currently no comments.');
-		}
-		?>
-	</ul>
-    <div id="commentform">
-
-		<br>
-<?php $post->comment_form()->out(); ?>
-	</div>
-</div> <!-- close comment div -->
+		</div>
+<!-- /comments -->
--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-/*
+<?php /*
 Copyright (c) 2012 Bryce Campbell
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -6,9 +6,15 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+*/ ?>
 <?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); } ?>
 <?php $theme->display( 'searchform' ); ?>
+<?php $about = Options::get( 'about' ); if( !empty( $about ) ) { ?>
+		<div class="sb-about">
+		<h2><?php _e('About'); ?></h2>
+		<p><?php echo $about; ?></p>
+		</div>
+<?php } ?>
 <div id="pages"> <!-- placeholder for pages -->
 <h2>Pages</h2>
 <ul class="noBullet">
@@ -20,11 +26,29 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 <?php } ?> <!-- end loop -->
 </ul>
 </div>
+<?php echo $theme->area( 'sidebar' ); ?>
+
+<div class="archive">
+		<h2>Monthly Archive</h2>
+		<?php echo $theme->monthly_archives(); ?>
+		</div>
+		<div class="recent">
+		<?php echo $theme->show_recentcomments(); ?>
+		</div>
+		<div class="tags">
+		<h2>Tags</h2>
+		<?php echo $theme->tag_cloud(); ?>
+		</div>
+		<div class="blogroll">
+<?php echo $theme->show_blogroll(); ?>
+		</div>
 <div id="meta">
 <h2>Meta</h2>
 <ul class="noBullet">
 <li><a class="feedlink" href="<?php URL::out( 'atom_feed', array( 'index' => 1)); ?>"><?php _e('Entry feed'); ?></a></li>
 <li><a class="feedlink" href="<?php URL::out( 'atom_feed_comments'); ?>"><?php _e('Comment feed'); ?></a></li>
+<li><a href="http://www.amazon.com/Bryces-Blog/dp/B007RHRWFK/ref=sr_1_1?s=digital-text&#38;ie=UTF8&#38;qid=1333640280&#38;sr=1-1">Subscribe via Amazon</a></li>
+<li><a href="http://www.google.com/producer/editions/CAow8dtA/bryces_blog">Subscribe via Google Currents</a></li>
 <?php if ($loggedin)
 { ?>
 	<li><a href="<?php Site::out_url( 'admin' ); ?>" title="<?php _e('Admin Area'); ?>"><?php _e('Admin'); ?></a></li>
