8bde9101b230ad81ad2a1c7d5463c63a0e436a48
Bryce <tonyhawk2100@gmail.com>
2013-04-30 10:19:24 -0700

    allow theme to be configurable

--- sidebar.php
+++ sidebar.php
@@ -9,12 +9,15 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 */ ?>
 <?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); } ?>
 <?php $theme->display( 'searchform' ); ?>
+<?php if (Options::get('display_about')) { ?> <!-- check if about should be displayed -->
 <?php $about = Options::get( 'about' ); if( !empty( $about ) ) { ?>
 		<div class="sb-about">
 		<h2><?php _e('About'); ?></h2>
 		<p><?php echo $about; ?></p>
 		</div>
-<?php } ?>
+<?php } ?> <!-- end nested if -->
+<?php } ?> <!-- end about if -->
+<?php if (Options::get('display_pages')) { ?> <!-- check if pages should be displayed -->
 <div id="pages"> <!-- placeholder for pages -->
 <h2>Pages</h2>
 <ul class="noBullet">
@@ -26,13 +29,17 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 <?php } ?> <!-- end loop -->
 </ul>
 </div>
+<?php } ?> <!-- end pages if -->
 <?php echo $theme->area( 'sidebar' ); ?>
 
+<?php if (Options::get('display_archive')) { ?> <!-- check if monthly archive should be displayed -->
 <div class="archive">
 		<h2>Monthly Archive</h2>
 		<?php echo $theme->monthly_archives(); ?>
 		</div>
-		<div class="recent">
+<?php } ?> <!-- end monthly archives if -->
+<?php if (Options::get('display_recent')) { ?> <!-- check if recent comments should be displayed -->
+<div class="recent">
         <ul>
 		<?php foreach($recent_comments as $comment) { ?>
         <li>
@@ -45,25 +52,39 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
         <?php } // end loop ?>
         </ul>
 		</div>
+        <?php } ?> <!-- end recent comments if -->
+        
+        <?php if (Options::get('tags')) { ?> <!-- check if tags should be displayed -->
 		<div class="tags">
 		<h2>Tags</h2>
 		<?php echo $theme->tag_cloud(); ?>
 		</div>
+        <?php } ?> <!-- end tags if -->
+        <?php if(Options::get('display_blogroll')) { ?> <!-- check if blogroll should be displayed -->
 		<div class="blogroll">
 <?php echo $theme->show_blogroll(); ?>
 		</div>
+        <?php } ?> <!-- end blogroll if -->
+<?php if(Options::get('display_flinks') || Options::get('display_mlinks') || Options::get('display_login')) { ?> <!-- make sure div is present only when needed -->
 <div id="meta">
 <h2>Meta</h2>
 <ul class="noBullet">
+<?php if(Options::get('display_flinks')) { ?> <!-- check if feed links should be displayed -->
 <li><a class="feedlink" href="<?php URL::out( 'atom_feed', array( 'index' => 1)); ?>"><?php _e('Entry feed'); ?></a></li>
 <li><a class="feedlink" href="<?php URL::out( 'atom_feed_comments'); ?>"><?php _e('Comment feed'); ?></a></li>
+<?php } ?> <!-- end feeds if -->
+<?php if(Options::get('display_mlinks')) { ?> <!-- check if misc links should be displayed -->
 <li><a href="http://www.amazon.com/Bryces-Blog/dp/B007RHRWFK/ref=sr_1_1?s=digital-text&#38;ie=UTF8&#38;qid=1333640280&#38;sr=1-1">Subscribe via Amazon</a></li>
 <li><a href="http://www.google.com/producer/editions/CAow8dtA/bryces_blog">Subscribe via Google Currents</a></li>
+<?php } ?> <!-- end misc if -->
+<?php if(Options::get('display_login')) { ?> <!-- check if login should be displayed -->
 <?php if ($loggedin)
 { ?>
 	<li><a href="<?php Site::out_url( 'admin' ); ?>" title="<?php _e('Admin Area'); ?>"><?php _e('Admin'); ?></a></li>
 <?php } else { ?>
 	<li><a href="<?php Site::out_url( 'login' ); ?>" title="<?php _e('Login'); ?>"><?php _e('Login'); ?></a></li>
   <?php } ?>
+<?php } ?> <!-- end main login if -->
 </ul>
 </div>
+<?php } else; ?> <!-- end if statement for meta div -->
--- theme.php
+++ theme.php
@@ -10,6 +10,58 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 <?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); }
 class rdarkness extends Theme
 {
+	// create options that will allow theme configuration
+	var $defaults = array(
+	'display_login' => true,
+	'display_rcomments' => true,
+	'display_pages' => true,
+	'display_archive' => true,
+	'display_tags' => true,
+	'display_blogroll' => true,
+	'display_about' => true,
+	'display_fmess' => true,
+	'display_mlinks' => true,
+	'display_flinks' => true,
+	);
+	
+	// set options upon activation
+	public function action_theme_activated()
+	{
+		$opts = Options::get_group(__CLASS__);
+		if (empty($opts))
+		{
+			Options::set_group(__CLASS__, $this->defaults);
+		}
+	}
+	
+	// allow theme configuration
+	public function action_theme_ui( $theme )
+	{
+		$ui = new FormUI('RDarkness config');
+		$login = $ui->append('checkbox', 'mod_login', 'display_login', _t('Display login'));
+		$login->value = Options::get('display_login');
+		$rcomments = $ui->append('checkbox', 'mod_rcomments', 'display_rcomments', _t('Display Recent Comments'));
+		$rcomments->value = Options::get('display_rcomments');
+		$pages = $ui->append('checkbox', 'mod_pages', 'display_pages', _t('Display Pages'));
+		$pages->value = Options::get('display_pages');
+		$archive = $ui->append('checkbox', 'mod_archives', 'display_archives', _t('Display Archives'));
+		$archive->value = Options::get('display_archives');
+		$dtags = $ui->append('checkbox', 'mod_tags', 'display_tags', _t('Display Tags'));
+		$dtags->value = Options::get('display_tags');
+		$blogroll = $ui->append('checkbox', 'mod_blogroll', 'display_blogroll', _t('Display Blogroll'));
+		$blogroll->value = Options::get('display_blogroll');
+		$mabout = $ui->append('checkbox', 'mod_mod_about', 'display_about', _t('Display About Message'));
+		$mabout->value = Options::get('display_about');
+		$fmess = $ui->append('checkbox', 'mod_fmess', 'display_fmess', _t('Display Footer Message'));
+		$fmess->value = Options::get('display_fmess');
+		$mlinks = $ui->append('checkbox', 'mod_mlinks', 'display_mlinks', _t('Display Misc. Links'));
+		$mlinks->value = Options::get('display_mlinks');
+		$flinks = $ui->append('checkbox', 'mod_flinks', 'display_flinks', _t('Display Feed Links'));
+		$flinks->value = Options::get('display_flinks');
+		$ui->append('submit', 'save', _t('Save'));
+		$ui->set_option('success_message', _t('Configuration saved'));
+		return $ui;
+	}
 	public function action_init_theme()
 	{
 		// get tags working
@@ -29,6 +81,7 @@ class rdarkness extends Theme
 		// add a recent comments variable, to remove need for plugin
 		if (!$this->template_engine->assign( 'recent_comments' ) )
 		{
+			// this limits the list of recent comments to five items
 			$this->assign( 'recent_comments', Comments::get( array('limit' => 5, 'status' => Comment::STATUS_APPROVED, 'type' => Comment::COMMENT, 'orderby' => 'date DESC' ) ) );
 		}
 		
